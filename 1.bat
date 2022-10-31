017f80a77328aa523fa4064857c795ad5b661332
AntonVoronezh <pstut@mail.ru>
2022-10-31 18:00:06 +0300

    init

--- 1.bat
+++ 1.bat
@@ -1,4 +1,4 @@
-md todo & cd todo
+                 md todo & cd todo
 mkdir templates\todo
 mkdir static\css
 echo .> static/css/style.css
@@ -11,9 +11,9 @@ echo .> database/__init__.py
 echo .> database/base.py
 echo .> models.py
 echo .> config.py
-echo .> main.py
+echo .> app.py
 echo .> routes.py
 echo .> templates/todo/layout.html
 echo .> templates/todo/index.html
 cd ..
-echo .> run.py
+echo .> main.py
