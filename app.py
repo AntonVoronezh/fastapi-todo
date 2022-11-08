acbc8754c1a8d6740bce55df99ac6136e273bbdc
AntonVoronezh <pstut@mail.ru>
2022-11-08 17:45:07 +0300

    init

--- app.py
+++ app.py
@@ -26,13 +26,19 @@ def get_all_items():
     return items
 
 
-@app.get('/item/{item_id}')
+@app.get('/item/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
 def get_item_by_id(item_id: int):
-    pass
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    return item
 
 
 @app.post('/items', response_model=Item, status_code=status.HTTP_201_CREATED)
 def create_item(item: Item):
+    db_item = db.query(models.Item).filter(models.Item.name == item.name).first()
+
+    if db_item is not None:
+        return HTTPException(status_code=400, detail='Name is !!!!!!!!!!!!!!!!!!!!!@!!!')
+
     new_item = models.Item(
         name=item.name,
         description=item.description,
@@ -40,25 +46,36 @@ def create_item(item: Item):
         on_offer=item.on_offer,
     )
 
-    db_item = db.query(models.Item).filter(item.name == new_item.name).first()
-
-    if db_item is not None:
-        return HTTPException(status_code=400, detail='Name is !!!!!!!!!!!!!!!!!!!!!@!!!')
-
     db.add(new_item)
     db.commit()
 
     return new_item
 
 
-@app.put('/item/{item_id}')
-def update_item_by_id(item_id: int):
-    pass
+@app.put('/item/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
+def update_item_by_id(item_id: int, item: Item):
+    item_to_update = db.query(models.Item).filter(models.Item.id == item_id).first()
+    item_to_update.name = item.name
+    item_to_update.price = item.price
+    item_to_update.on_offer = item.on_offer
+    item_to_update.description = item.description
+
+    db.commit()
+
+    return item
 
 
 @app.delete('/item/{item_id}')
 def delete_item_by_id(item_id: int):
-    pass
+    item_to_delete = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    if item_to_delete is None:
+        raise HTTPException(status_code=404, detail='** not found **')
+
+    db.delete(item_to_delete)
+    db.commit()
+
+    return item_to_delete
 
 # @app.get('/')
 # def index():
